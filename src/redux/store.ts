e9a7aa2340f7fad45be0ce584f3c68c61e70d06b
Karol Guzikowski <karolguzikowski@gmail.com>
2020-05-10 15:10:58 +0200

    removed redux-devtools-extension

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,11 +1,10 @@
 import { createStore, applyMiddleware } from 'redux'
-import { composeWithDevTools } from 'redux-devtools-extension';
 import rootReducer from './rootReducer'
 import  thunk from 'redux-thunk'
 import { persistStore } from 'redux-persist'
 
 const middlewares = [thunk]
 
-export const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(...middlewares)))
+export const store = createStore(rootReducer, applyMiddleware(...middlewares))
 
 export const persistor = persistStore(store)
